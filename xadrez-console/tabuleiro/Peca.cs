93d6701c3f4f13e9e85d82b6f95d6378c1196763
Renato Douglas <renatdoug@hotmail.com>
2022-12-21 15:59:54 -0300

    Alterado a variavel tabulaeiro para tb

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -8,12 +8,12 @@ namespace tabuleiro
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
         public int qteMovimentos { get; protected set; }
-        public Tabuleiro tabuleiro { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
 
         public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
         {
             this.posicao = posicao;
-            this.tabuleiro = tab;
+            this.tab = tab;
             this.cor = cor;
             this.qteMovimentos= 0;
         }
